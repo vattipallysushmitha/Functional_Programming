a99af51f37941119271fa0779de19a3f87b2ac5f
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-07-20 13:14:33 +0530

    added distinct triplets

--- src/com/bridzelab/functionalProgramming/FunctionalProgramming.java
+++ src/com/bridzelab/functionalProgramming/FunctionalProgramming.java
@@ -1,17 +1,50 @@
 package com.bridzelab.functionalProgramming;
-import java.util.Scanner;   // import Scanner
+import java.util.Arrays;  //Import arrays
+import java.util.Scanner; // Import Scanner
 public class FunctionalProgramming {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);   // Make Scanner Obj
-        System.out.println("Enter the co-ordinates x,y: ");
-        int x = scanner.nextInt();  // Input Int
-        int y = scanner.nextInt();
+	static void findTriplets(int[] arr, int n)  //Initialize with function with 2 parameters.
+    {
+        int numberOfDistinctTriples = 0;
+        boolean found = false;     // Initialize condition
+        for (int i=0; i<n-2; i++)     // Nested for loop
+        {
+            for (int j=i+1; j<n-1; j++)
+            {
+                for (int k=j+1; k<n; k++)
+                {
+                    if (arr[i]+arr[j]+arr[k] == 0)
+                    {
+                        System.out.print(arr[i]+" ");  // 1st element
+                        System.out.print(arr[j]+" ");  // 2nd element
+                        System.out.print(arr[k]+" "+"\n"); //3rd element
+                        numberOfDistinctTriples++;
 
-        double distance = Math.sqrt(x*x+y*y);  //Mathematical Formula
-        System.out.println("The Euclidean Distance of the point from origin is = "+ distance);
+                        found = true;
+                    }
+                }
+            }
+        }
+        if (found == false) {
+            System.out.println(" not exist ");
+        }
+        System.out.println("number of distinct triples = "+numberOfDistinctTriples);
+    }
 
+    public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);  // Make Scanner Obj
+        System.out.println("Enter no of integers in the array: ");
+        int N = scanner.nextInt();
+        int[] array = new int[N];
+
+        System.out.println("Enter the integer values in the array: ");
+        for (int i=0; i<N;i++){
+            array[i]=scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(array)); //TypeCasting
+
+        findTriplets(array, N);  //Calling Function with 2 Parameters
     }
 
 }
